ee3a57f6738f2efa45155f432d2411db81b5fe88
JesseG <j.a.grant27@gmail.com>
2022-06-22 21:19:23 -0400

    ContactPage

--- Desktop/NucampFolder/3-React/nucampsite/src/App.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/App.js
@@ -1,13 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route
+          path='directory/:campsiteId'
+          element={<CampsiteDetailPage />}
+        />
+        <Route path='about' element={<AboutPage />} />
+      </Routes>
       <Footer />
     </div>
   );
