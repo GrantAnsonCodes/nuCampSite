958ce595cb3e087b7884c588ead04ae0ff0542e5
JesseG <j.a.grant27@gmail.com>
2022-08-01 19:41:32 -0400

    React Native Workshop Assignment Week 2

--- Desktop/NucampFolder/4-React-Native.nosync/nucampsite/features/comments/commentsSlice.js
+++ Desktop/NucampFolder/4-React-Native.nosync/nucampsite/features/comments/commentsSlice.js
@@ -9,24 +9,45 @@ export const fetchComments = createAsyncThunk(
     }
 );
 
-const commentsSlice = createSlice({
-    name: 'comments',
-    initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
-    extraReducers: {
-        [fetchComments.pending]: (state) => {
-            state.isLoading = true;
-        },
-        [fetchComments.fulfilled]: (state, action) => {
-            state.isLoading = false;
-            state.errMess = null;
-            state.commentsArray = action.payload;
-        },
-        [fetchComments.rejected]: (state, action) => {
-            state.isLoading = false;
-            state.errMess = action.error ? action.error.message : 'Fetch failed';
-        }
-    }
-});
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    async (payload, { dispatch, getState }) => {
+        setTimeout(() => {
+            setTimeout(() => {
+                const { comments } = getState();
+                const payload={ ...payload, id, date };
+                const d = new Date();
+                payload.date = d.toISOString();
+                dispatch(addComment(payload));
+            }, 2000);
+        });
+    })
 
-export const commentsReducer = commentsSlice.reducer;
+        const commentsSlice = createSlice({
+            name: 'comments',
+            initialState: { isLoading: true, errMess: null, commentsArray: [] },
+            reducers: {
+                addComment: (state, action) => {
+                    const commentsArray = state.commentsArray;
+                    commentsArray.push(action.payload);
+                },
+            },
+            extraReducers: {
+                [fetchComments.pending]: (state) => {
+                    state.isLoading = true;
+                },
+                [fetchComments.fulfilled]: (state, action) => {
+                    state.isLoading = false;
+                    state.errMess = null;
+                    state.commentsArray = action.payload;
+                },
+                [fetchComments.rejected]: (state, action) => {
+                    state.isLoading = false;
+                    state.errMess = action.error ? action.error.message : 'Fetch failed';
+                }
+            }
+        });
+
+        export const { addComment } = commentsSlice.actions;
+        export const commentsReducer = commentsSlice.reducer;
+
--- Desktop/NucampFolder/4-React-Native.nosync/nucampsite/screens/CampsiteInfoScreen.js
+++ Desktop/NucampFolder/4-React-Native.nosync/nucampsite/screens/CampsiteInfoScreen.js
@@ -1,7 +1,10 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Button, Modal } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
 import { toggleFavorite } from '../features/favorites/favoritesSlice';
+import { useState } from 'react';
+import { Input, Rating } from 'react-native-elements';
+import { postComment } from '../features/comments/commentsSlice';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
@@ -9,42 +12,122 @@ const CampsiteInfoScreen = ({ route }) => {
     const favorites = useSelector((state) => state.favorites);
     const dispatch = useDispatch();
 
+    const [showModal, setShowModal] = useState(false);
+
+    const [rating, setRating] = useState(5);
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
+
+    const handleSubmit = () => {
+        const newComment = {
+            campsiteId,
+            rating,
+            author,
+            text
+        };
+        dispatch(postComment(newComment));
+        setShowModal(!showModal);
+    };
+
+    const resetForm = () => {
+        rating(5);
+        author('');
+        text('');
+    };
+
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
-                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Rating
+                    startingValue={item.rating}
+                    imageSize={10}
+                    style={{ alignItems: 'flex-start', paddingVertical: '5%' }}
+                    readonly>{item.rating} Stars</Rating>
                 <Text style={{ fontSize: 12 }}>
                     {`-- ${item.author}, ${item.date}`}
                 </Text>
             </View>
         );
-    };
+    }
 
     return (
-        <FlatList
-            data={comments.commentsArray.filter(
-                (comment) => comment.campsiteId === campsite.id
-            )}
-            renderItem={renderCommentItem}
-            keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={{
-                marginHorizontal: 20,
-                paddingVertical: 20
-            }}
-            ListHeaderComponent={
-                <>
-                    <RenderCampsite
-                        campsite={campsite}
-                        isFavorite={favorites.includes(campsite.id)}
-                        markFavorite={() => dispatch(toggleFavorite(campsite.id))}
-                    />
-                    <Text style={styles.commentsTitle}>Comments</Text>
-                </>
-            }
-        />
+        <>
+            <FlatList
+                data={comments.commentsArray.filter(
+                    (comment) => comment.campsiteId === campsite.id
+                )}
+                renderItem={renderCommentItem}
+                keyExtractor={(item) => item.id.toString()}
+                contentContainerStyle={{
+                    marginHorizontal: 20,
+                    paddingVertical: 20
+                }}
+                ListHeaderComponent={
+                    <>
+                        <RenderCampsite
+                            campsite={campsite}
+                            isFavorite={favorites.includes(campsite.id)}
+                            markFavorite={() => dispatch(toggleFavorite(campsite.id))}
+                            onShowModal={() => setShowModal(!showModal)}
+                        />
+                        <Text style={styles.commentsTitle}>Comments</Text>
+                    </>
+                }
+            />
+
+            <Modal
+                animationType='slide'
+                transparent={false}
+                visible={showModal}
+                onRequestClose={() => setShowModal(!showModal)}
+            >
+                <View style={styles.modal}>
+                    <Rating
+                        showRating
+                        startingValue={rating}
+                        imageSize={40}
+                        onFinishRating={(rating) => setRating(rating)}
+                        style={{ paddingVertical: 10 }} />
+                    <Input
+                        placeholder='Author'
+                        leftIcon='user-o'
+                        leftIconContainerStyle={{ paddingRight: 10 }}
+                        onChangeText={(text) => setText(text)}
+                        value={this.state.value} />
+                    <Input
+                        placeholder='Comment'
+                        leftIcon='comment-o'
+                        leftIconContainerStyle={{ paddingRight: 10 }}
+                        onChangeText={(text) => setText(text)}
+                        value={this.state.value} />
+                    <Button />
+                    <View style={{ margin: 10 }}>
+                        <Button
+                            title='Submit'
+                            color='#5637DD'
+                            onPress={() => {
+                                handleSubmit();
+                                resetForm();
+                            }}
+                        />
+                    </View>
+                    <View style={{ margin: 10 }}>
+                        <Button
+                            title='Cancel'
+                            color='#808080'
+                            onPress={() => {
+                                setShowModal(!showModal);
+                                resetForm();
+                            }}
+                        />
+                    </View>
+                </View>
+            </Modal>
+        </>
     );
-};
+}
+
 
 const styles = StyleSheet.create({
     commentsTitle: {
@@ -60,6 +143,10 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 20,
         backgroundColor: '#fff'
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
     }
 });
 
